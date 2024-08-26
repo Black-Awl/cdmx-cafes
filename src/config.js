c4f449c29778f5b231995fab64f6e1fd13741a5c
Kevin N. Murphy <knmurphy@users.noreply.github.com>
2024-08-26 15:49:38 -0600

    new astro project Signed-off-by: Kevin N. Murphy <knmurphy@users.noreply.github.com>

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const AIRTABLE_API_KEY = import.meta.env.AIRTABLE_API_KEY;
+export const AIRTABLE_BASE_ID = import.meta.env.AIRTABLE_BASE_ID;
+export const AIRTABLE_TABLE_NAME = import.meta.env.AIRTABLE_TABLE_NAME;
+export const PUBLIC_MAPTILER_KEY = import.meta.env.PUBLIC_MAPTILER_KEY;
