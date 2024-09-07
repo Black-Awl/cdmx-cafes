c39e845e039b4f87574ea6b20c87353380d853a0
Kevin N. Murphy <knmurphy@users.noreply.github.com>
2024-09-07 10:45:15 -0600

    complete rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import astroI18n from "astro-i18n-aut/integration";
 
+// https://astro.build/config
 export default defineConfig({
-  // Your configuration options here
+  integrations: [
+    tailwind(),
+    astroI18n({
+      defaultLanguage: "en",
+      supportedLanguages: ["en", "es"],
+      translationDirectory: "./src/locales",
+    }),
+  ],
 });
